c7c26b232635b1f57d56a5bcf117bcb9c8c98759
Andrey V Shamin <addsharming@mail.ru>
2021-09-10 15:39:52 +0300

    Programs to collect information from several different excel files to sqlite db and csv to send via corporate gosuslugi. Functions to save information about SIM (glonass on vehicles and spare glonass) were added to program. Version 13.08.2021.

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from openpyxl import load_workbook
     - номер телефона (num_tel)
     - номер симкарты (num_sim) (серийный номер)
     - назначение симкарты (purpose) (ГЛОНАСС/GPS АРМ, ГЛОНАСС/GPS АВТОМОБИЛИ, ГЛОНАСС/GPS ЗАПАСНЫЕ,
-        СЕТЕВОЙ МОНИТОРИНГ, ОПРОС CSD СЧЕТЧИКОВ, CSD НА СЧЕТЧИКАХ, LORA, Телемеханика)
+        СЕТЕВОЙ МОНИТОРИНГ, ОПРОС CSD СЧЕТЧИКОВ, CSD НА СЧЕТЧИКАХ, LORA, Телемеханика, LORA/Телемеханика запасные,
+        Личный кабинет, Интернет)
     - Адрес, № ТП, населённый пункт, Точка учёта и т. д. (set_addr) (б-р 50 лет Октября д.14,
                                                             г. Брянск пер Пилотов    Ввод 2,
                                                             Брянск г, Вокзальная ул, дом № 134)
@@ -26,7 +27,8 @@ from openpyxl import load_workbook
     - номер телефона (num_tel)
     - номер симкарты (num_sim) (серийный номер)
     - назначение симкарты (purpose) (ГЛОНАСС/GPS АРМ, ГЛОНАСС/GPS АВТОМОБИЛИ, ГЛОНАСС/GPS ЗАПАСНЫЕ,
-        СЕТЕВОЙ МОНИТОРИНГ, ОПРОС CSD СЧЕТЧИКОВ, CSD НА СЧЕТЧИКАХ, LORA, Телемеханика)
+        СЕТЕВОЙ МОНИТОРИНГ, ОПРОС CSD СЧЕТЧИКОВ, CSD НА СЧЕТЧИКАХ, LORA, Телемеханика, LORA/Телемеханика запасные,
+        Личный кабинет, Интернет)
     - Адрес, № ТП, населённый пункт, Точка учёта и т. д. (set_addr) (б-р 50 лет Октября д.14,
                                                             г. Брянск пер Пилотов    Ввод 2,
                                                             Брянск г, Вокзальная ул, дом № 134)
@@ -40,7 +42,8 @@ from openpyxl import load_workbook
     - номер телефона (num_tel)
     - номер симкарты (num_sim) (серийный номер)
     - назначение симкарты (purpose) (ГЛОНАСС/GPS АРМ, ГЛОНАСС/GPS АВТОМОБИЛИ, ГЛОНАСС/GPS ЗАПАСНЫЕ,
-        СЕТЕВОЙ МОНИТОРИНГ, ОПРОС CSD СЧЕТЧИКОВ, CSD НА СЧЕТЧИКАХ, LORA, Телемеханика)
+        СЕТЕВОЙ МОНИТОРИНГ, ОПРОС CSD СЧЕТЧИКОВ, CSD НА СЧЕТЧИКАХ, LORA, Телемеханика, LORA/Телемеханика запасные,
+        Личный кабинет, Интернет)
     - Адрес, № ТП, населённый пункт, Точка учёта и т. д. (set_addr) (б-р 50 лет Октября д.14,
                                                             г. Брянск пер Пилотов    Ввод 2,
                                                             Брянск г, Вокзальная ул, дом № 134)
@@ -67,6 +70,36 @@ from openpyxl import load_workbook
 6. Таблица СИМ карт ТЕЛЕ2 (tele2_operator)
     - номер телефона (num_tel)
     - номер симкарты (num_sim)
+    
+--   
+select * from mts_operator where num_tel IN
+(select num_tel from mts_operator where date = '2021-08-13'
+except
+select num_tel from mts_current where date = '2021-06-15'); 2332 шт
+--
+select * from mts_operator where num_tel IN
+(select num_tel from mts_operator where date = '2021-08-13'
+except
+select num_tel from mts_current where date = '2021-06-15')
+and date = '2021-08-13';                                    1316 шт
+--    
+select num_tel from mts_current where date = '2021-06-15';  7674 шт
+--
+select distinct(num_tel) from mts_operator where num_tel IN
+(select num_tel from mts_operator where date = '2021-08-13'
+except
+select num_tel from mts_current where date = '2021-06-15'); 1316 шт
+--
+select num_tel from mts_operator where date = '2021-08-13'; 8971 шт 
+--
+select distinct(num_tel) from mts_operator where date = '2021-06-15'; 8671 шт
+--
+select distinct(num_tel) from mts_operator where date = '2021-08-13'; 8971 шт 
+--
+select num_tel from mts_operator where date = '2021-08-13'
+except
+select num_tel from mts_operator where date = '2021-06-15'; 300 шт
+    
 """
 
 def make_db():
@@ -131,16 +164,17 @@ def make_db():
 
 READFIRSTMTS: bool = False
 READFIRSTMEGAFON: bool = False
+READFIRSTMTS210813: bool = False
 
-def mts_site_to_db(excel_filename: str, sheet: str, db_filename: str, columns_to_readwrite: list):
+def mts_site_to_db(excel_filename: str, sheet: str, db_filename: str, columns_to_readwrite: list,
+                   date: str = '2021-06-15'):
     wb = load_workbook(excel_filename)
     #print(wb.sheetnames)
     sheet = wb['Charges']
     #print(sheet.title)
     col = 1
-    date = '2021-06-15'
     opsos = 'MTS'
-    for i in range(2, 9500):
+    for i in range(2, 11500):
         num = sheet.cell(row=i, column=1).value
         if num:
             row = [col, num, date, opsos, ]
@@ -150,7 +184,7 @@ def mts_site_to_db(excel_filename: str, sheet: str, db_filename: str, columns_to
                     val = tval
                 row.append(tval)
             col += 1
-            conn = sqlite3.connect("simdatabase.db")
+            conn = sqlite3.connect(db_filename)
             cursor = conn.cursor()
             sql = f"""INSERT INTO mts_operator(num_tel, account, date) VALUES ({row[1]}, {row[4]}, "{row[2]}")"""
             #print(sql)
@@ -159,6 +193,75 @@ def mts_site_to_db(excel_filename: str, sheet: str, db_filename: str, columns_to
             conn.commit()
     cursor.close()
     conn.close()
+
+def mts_site_charge_report_electric_energy_meters_15_06_2021_to_csv():
+    excel_filename = "charge_report_electric_energy_meters_15_06_2021.xlsx"
+    wb = load_workbook(excel_filename)
+    sheet = wb['Charges']
+    col = 1
+    page = 1
+    for i in range(2, 11500):
+        file = f'sim-piramida-asque-meter_15_06_2021_{page}.csv'
+        if col == 1:
+            csv_file = open(file, "w", newline='', encoding='utf-8')
+            writer = csv.writer(csv_file, delimiter=';')
+            writer.writerow(['Наименование устройства',
+                             'Идентификационный номер',
+                             'Тип устройства',
+                             'Номер телефона',
+                             'Адрес, где находится устройство'])
+        t_num = sheet.cell(row=i, column=1).value
+        if t_num:
+            num = t_num[1:]
+            auxiliary = f"Счетчик_{page}_{col}"
+            snum = ""
+            addr = "Г Брянск Станке Димитрова 5В АСКУЭ"
+            csv_row = [auxiliary, snum, 'Модем', num, addr]
+            print(col, csv_row)
+            writer.writerow(csv_row)
+            col += 1
+        if col == 900:
+            col = 1
+            page += 1
+            csv_file.close()
+    csv_file.close()
+
+
+def mts_site210813_minus_current210615_to_csv():
+    conn = sqlite3.connect("simdatabase.db")
+    type = "Прочее"
+    t_auxiliary = "МТС SIM резерв"
+    addr = "г. Брянск, пр-т Станке Димтрова, 5в, АСКУЭ"
+    snum = ""
+    num = ""
+    cwd = os.getcwd()
+    #print(cwd)
+    os.chdir("C:/Users/shamin.a/PycharmProjects/simcard")
+    file = 'sim-mts_site210813_minus_current210615.csv'
+    cursor = conn.cursor()
+    sql = f"""select distinct(num_tel) from mts_operator where num_tel IN
+(select num_tel from mts_operator where date = '2021-08-13'
+except
+select num_tel from mts_current where date = '2021-06-15');  """
+    print(sql)
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    with open(file, "w", newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow(['Наименование устройства',
+                         'Идентификационный номер',
+                         'Тип устройства',
+                         'Номер телефона',
+                         'Адрес, где находится устройство'])
+        for i, t_num in enumerate(data):
+            num = str(t_num[0])[1:]
+            auxiliary = f"{t_auxiliary} {i}"
+            row = [auxiliary, snum, type, num, addr]
+            print(row)
+            writer.writerow(row)
 
 def mts_on_piramida_server_to_csv_db(excel_filename: str = "", db_filename: str = "", db_table:str = "",
                                      columns_to_readwrite: list = ["", ]):
@@ -393,6 +496,52 @@ def mts_glonass_spare_to_csv_db(excel_filename: str = "", db_filename: str = "",
     cursor.close()
     conn.close()
 
+def mts_netping_to_csv_db(excel_filename: str = "", db_filename: str = "", db_table:str = "",
+                                     columns_to_readwrite: list = ["", ]):
+    date = "2021-06-15"
+    purpose = "СЕТЕВОЙ МОНИТОРИНГ"
+    type = "Прочее"
+    cwd = os.getcwd()
+    #print(cwd)
+    os.chdir("C:/Users/shamin.a/PycharmProjects/simcard")
+    file = 'sim-netping_15_06_2021.csv'
+    wb = load_workbook('SIM-карты ГЛОНАСС АСКУЭ LoRa.xlsx')
+    #print(wb.sheetnames)
+    sheet = wb['NetPing']
+    #print(sheet.title)
+    addr = "Брянск Станке Димитрова 5В"
+    tauxiliary = "Мониторинг серверных помещений"
+    col = 0
+    with open(file, "w", newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow(['Наименование устройства',
+                         'Идентификационный номер',
+                         'Тип устройства',
+                         'Номер телефона',
+                         'Адрес, где находится устройство'])
+        for i in range(5, 8500):
+            num = sheet.cell(row=i, column=3).value
+            if num:
+                snum = sheet.cell(row=i, column=4).value
+                if snum is None: snum = ''
+                tnum_sim = sheet.cell(row=i, column=5).value
+                num_sim = int(tnum_sim.replace('-', '')) if tnum_sim is not None else 0
+                num = str(num)[1:]
+                auxiliary = f"{tauxiliary} {col}"
+                col += 1
+                row = [auxiliary, snum, type, num, addr]
+                writer.writerow(row)
+                conn = sqlite3.connect(db_filename)
+                cursor = conn.cursor()
+                sql = f"""INSERT INTO {db_table}(num_tel, num_sim, purpose, set_addr, aux, type, s_num, date)
+                VALUES (7{row[3]}, {num_sim}, '{purpose}', '{addr}', '{auxiliary}', '{row[2]}', '{row[1]}', '{date}')"""
+                #print(sql)
+                cursor.execute(sql)
+                #print(f"{col} {row}")
+                conn.commit()
+    cursor.close()
+    conn.close()
+
 
 def megafon_site(excel_filename: str, db_filename: str, columns_to_readwrite: list):
     wb = load_workbook(MEGAFONFIRST)
@@ -445,6 +594,67 @@ def megafon_to_csv(excel_filename: str = "", db_filename: str = "", columns_to_r
                 writer.writerow([auxiliary, snum, 'Модем', num, "Брянск, " + addr])
 
 
+def tele2_site_to_csv_db(excel_filename: str = "", db_filename: str = "", db_table:str = "",
+                                     columns_to_readwrite: list = ["", ]):
+    date = "2021-06-15"
+    purpose = "LORA"
+    type = "Модем"
+    cwd = os.getcwd()
+    #print(cwd)
+    os.chdir("C:/Users/shamin.a/PycharmProjects/simcard")
+    file = 'sim-tele2_15_06_2021.csv'
+    wb = load_workbook('tele2_M2M_lk_sim_15_06_2021.xlsx')
+    #print(wb.sheetnames)
+    sheet = wb['Лист1']
+    #print(sheet.title)
+
+    col = 0
+    with open(file, "w", newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow(['Наименование устройства',
+                         'Идентификационный номер',
+                         'Тип устройства',
+                         'Номер телефона',
+                         'Адрес, где находится устройство'])
+        for i in range(2, 8500):
+            num = sheet.cell(row=i, column=1).value
+            if num:
+                taddr = sheet.cell(row=i, column=4).value
+                if taddr: addr = taddr
+                t_auxiliary = sheet.cell(row=i, column=2).value
+                if t_auxiliary: auxiliary = t_auxiliary
+                t_type = sheet.cell(row=i, column=5).value
+                if t_type: type = t_type
+                if auxiliary[:6] == "Резерв":
+                    purpose = "LORA/Телемеханика запасные"
+                elif auxiliary[-10:] == "мониторинг":
+                    purpose = "СЕТЕВОЙ МОНИТОРИНГ"
+                elif auxiliary[:4] == "LoRa":
+                    purpose = "LORA"
+                elif auxiliary[:2] == "ЛК":
+                    purpose = "Личный кабинет"
+                elif auxiliary[-9:] == "Интернета":
+                    purpose = "Интернет"
+                else:
+                    purpose = "Телемеханика"
+                snum = sheet.cell(row=i, column=3).value
+                num = str(num)[2:].strip().replace("-", "").replace(" ", "")
+                col += 1
+                row = [auxiliary, snum, type, num, addr]
+                writer.writerow(row)
+                conn = sqlite3.connect(db_filename)
+                cursor = conn.cursor()
+                sql = f"""INSERT INTO {db_table}(num_tel, purpose, set_addr, aux, type, s_num, date)
+                VALUES (7{row[3]}, '{purpose}', '{addr}', '{auxiliary}', '{row[2]}', '{row[1]}', '{date}')"""
+                print(sql)
+                #cursor.execute(sql)
+                print(f"{col} {row}")
+                conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
 if __name__ == '__main__':
     cwd = os.getcwd()
     #print(cwd)
@@ -462,11 +672,15 @@ if __name__ == '__main__':
         mts_on_server_modem_and_region_to_csv_db("sim-server-arm-asque_15_06_2021.csv", "simdatabase.db", "mts_current")
         mts_on_glonass_vehicle_to_csv_db("sim-glonass-vehicle_15_06_2021.csv", "simdatabase.db", "mts_current")
         mts_glonass_spare_to_csv_db("sim-glonass-spare_15_06_2021.csv", "simdatabase.db", "mts_current")
+        mts_netping_to_csv_db("sim-netping_15_06_2021.csv", "simdatabase.db", "mts_current")
+    if READFIRSTMTS210813:
+        mts_site_to_db("charge_report_13_08_2021.xlsx", "Charges", "simdatabase.db", [6, ], '2021-08-13')
     if READFIRSTMEGAFON:
         MEGAFONFIRST: str = 'mobileSubscribers_20210723_140421_15_06_2021.xlsx'
         megafon_site(MEGAFONFIRST, 'simdatabase.db', [7, ])
     #megafon_to_csv()
-
-
+    #tele2_site_to_csv_db("sim-tele2_15_06_2021.csv", "simdatabase.db", "tele2_current")
+    #mts_site210813_minus_current210615_to_csv()
+    mts_site_charge_report_electric_energy_meters_15_06_2021_to_csv()
 
 
